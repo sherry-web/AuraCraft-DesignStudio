075bb96ea493b69392e3c20a56aafd0f7bd01f5b
sherry-web <72515795+sherry-web@users.noreply.github.com>
2024-07-08 19:36:01 +0200

    Error: Resolve CSS errors due to missing image files

--- auracraft-design-studio/src/components/Home.js
+++ auracraft-design-studio/src/components/Home.js
@@ -2,9 +2,435 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to AuraCraft Design Studio</h1>
-      <p>Your partner in creating exceptional UX/UI designs.</p>
+
+    <div className='main-container'>
+      <div className='selected-icons'>
+        <div className='logo' />
+        <div className='navbar'>
+          <div className='item'>
+            <span className='home'>Home</span>
+          </div>
+          <div className='about-us'>
+            <span className='item-1'>About Us</span>
+          </div>
+          <div className='frame'>
+            <div className='services'>
+              <span className='menu-icon'>Services</span>
+              <div className='union'>
+                <div className='item-2' />
+              </div>
+            </div>
+          </div>
+          <div className='portfolio'>
+            <span className='item-3'>Portfolio</span>
+          </div>
+          <div className='contact-us'>
+            <span className='frame-4'>Contact Us</span>
+          </div>
+        </div>
+        <div className='button'>
+          <button className='state-layer'>
+            <div className='label-text'>
+              <span className='icons'>Start Project</span>
+            </div>
+          </button>
+          <div className='icons-5'>
+            <div className='menu'>
+              <div className='vector'>
+                <div className='flex-row-cfb' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='frame-6'>
+        <div className='frame-7'>
+          <div className='frame-8'>
+            <div className='iconoir-design-nib'>
+              <div className='group'>
+                <div className='frame-9' />
+              </div>
+            </div>
+            <div className='discover-ux-ui-services'>
+              <span className='lorem-ipsum'>Discover UX/UI Services</span>
+              <span className='button-a'>
+                Lorem ipsum dolor sit amet consectetur.
+              </span>
+            </div>
+            <div className='icon-wrapper'>
+              <div className='wrapper'>
+                <div className='union-b'>
+                  <div className='a' />
+                </div>
+              </div>
+              <span className='frame-c'>A</span>
+            </div>
+          </div>
+          <div className='frame-d'>
+            <div className='material-symbols-light'>
+              <div className='vector-e'>
+                <div className='frame-f' />
+              </div>
+            </div>
+            <div className='explore-front-end'>
+              <span className='lorem-ipsum-10'>
+                Explore Front-End Solutions
+              </span>
+              <span className='button-11'>
+                Lorem ipsum dolor sit amet consectetur.
+              </span>
+            </div>
+            <div className='icon-wrapper-12'>
+              <div className='wrapper-13'>
+                <div className='union-14'>
+                  <div className='pic-5' />
+                </div>
+              </div>
+              <span className='span-a'>A</span>
+            </div>
+          </div>
+          <div className='div-frame'>
+            <div className='div-frame-15'>
+              <div className='div-frame-16'>
+                <span className='span-explore-more'>Explore More</span>
+              </div>
+              <div className='div-button'>
+                <div className='div-icon-wrapper'>
+                  <div className='div-wrapper'>
+                    <div className='div-union' />
+                  </div>
+                </div>
+                <span className='span-a-17'>A</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='div-frame-18'>
+          <div className='div-frame-19'>
+            <span className='span-transform-your-vision'>
+              Transform Your Vision into Reality
+            </span>
+            <span className='text-10'>
+              Specializing in UX/UI Design and Front-End Development
+            </span>
+            <div className='div-frame-1a'>
+              <button className='button-button'>
+                <span className='span-text'>Our Projects</span>
+              </button>
+              <button className='button-button-1b'>
+                <span className='span-text-1c'>Get a Quote</span>
+              </button>
+            </div>
+          </div>
+          <div className='div-rectangle' />
+        </div>
+      </div>
+      <div className='div-flex-row-fe'>
+        <div className='div-frame-1d'>
+          <div className='div-frame-1e'>
+            <span className='span-portfolio'>Portfolio</span>
+            <span className='text-14'>
+              Expert UX/UI Design and Front-End Development
+            </span>
+            <button className='button-button-1f'>
+              <span className='span-text-20'>View More</span>
+            </button>
+          </div>
+        </div>
+        <div className='div-rectangle-21'>
+          <div className='div-frame-22'>
+            <span className='span-work-with-us'>Work With Us</span>
+            <span className='text-17'>
+              Expert UX/UI Design and Front-End Development
+            </span>
+            <button className='button-button-23'>
+              <span className='span-text-24'>Read More</span>
+            </button>
+          </div>
+        </div>
+        <div className='div-rectangle-25'>
+          <div className='div-frame-26'>
+            <span className='span-about-us'>About Us</span>
+            <span className='text-1a'>
+              Expert UX/UI Design and Front-End Development
+            </span>
+            <button className='button-button-27'>
+              <span className='span-text-28'>Read More</span>
+            </button>
+          </div>
+        </div>
+        <div className='div-rectangle-29'>
+          <div className='div-rectangle-2a'>
+            <span className='span-mock-up-images'>Mock up images</span>
+          </div>
+        </div>
+        <div className='div-rectangle-2b'>
+          <div className='div-rectangle-2c'>
+            <span className='span-mock-up-images-2d'>Mock up images</span>
+          </div>
+        </div>
+        <div className='div-rectangle-2e'>
+          <div className='div-rectangle-2f' />
+          <span className='span-mock-up-images-30'>Mock up images</span>
+        </div>
+      </div>
+      <div className='div-flex-row-d'>
+        <div className='div-rectangle-31'>
+          <span className='user-experience-design'>
+            What is User eXperience /User Interface Design?
+          </span>
+          <div className='frame-32'>
+            <div className='frame-33'>
+              <div className='rectangle' />
+              <div className='rectangle-34' />
+            </div>
+            <div className='frame-35'>
+              <div className='rectangle-36' />
+              <div className='rectangle-37' />
+            </div>
+          </div>
+        </div>
+        <div className='rectangle-38'>
+          <span className='front-end-development'>
+            What is Front-End Development?
+          </span>
+          <div className='frame-39'>
+            <div className='frame-3a'>
+              <div className='rectangle-3b' />
+              <div className='rectangle-3c' />
+            </div>
+            <div className='frame-3d'>
+              <div className='rectangle-3e' />
+              <div className='rectangle-3f' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='rectangle-40'>
+        <div className='frame-41'>
+          <div className='frame-42'>
+            <div className='frame-43'>
+              <span className='our-process'>Our Process</span>
+              <div className='frame-44'>
+                <div className='frame-45'>
+                  <div className='rectangle-46' />
+                  <div className='frame-47'>
+                    <span className='step-1'>Step 1</span>
+                    <span className='description'>Description</span>
+                  </div>
+                </div>
+                <div className='frame-48'>
+                  <div className='rectangle-49' />
+                  <div className='frame-4a'>
+                    <span className='step-2'>Step 2</span>
+                    <span className='description-4b'>Description</span>
+                  </div>
+                </div>
+                <div className='frame-4c'>
+                  <div className='rectangle-4d' />
+                  <div className='frame-4e'>
+                    <span className='step-3'>Step 3</span>
+                    <span className='description-4f'>Description</span>
+                  </div>
+                </div>
+                <div className='frame-50'>
+                  <div className='rectangle-51' />
+                  <div className='frame-52'>
+                    <span className='step-4'>Step 4</span>
+                    <span className='description-53'>Description</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='rectangle-54'>
+        <div className='frame-55'>
+          <div className='frame-56'>
+            <div className='frame-57'>
+              <span className='auracraft-studio'>AURACRAFT STUDIO</span>
+              <span className='design-your-digital-footprint'>
+                design your digital footprint
+              </span>
+              <button className='button-58'>
+                <div className='state-layer-59'>
+                  <div className='label-text-5a'>
+                    <span className='try-now'>Try </span>
+                    <span className='aura-craft-design-studio'>
+                      AuraCraft-Design Studio
+                    </span>
+                    <span className='now'>Now</span>
+                  </div>
+                </div>
+              </button>
+            </div>
+            <div className='frame-5b'>
+              <button className='rectangle-5c' />
+              <span className='lorem-ipsum-text'>
+                Lorem ipsum dolor sit amet consectetur. Arcu ac pellentesque
+                justo nulla aliquet euismod quis orci purus.
+              </span>
+              <div className='frame-5d'>
+                <button className='rectangle-5e' />
+                <div className='frame-5f'>
+                  <span className='user-name'>User name</span>
+                  <span className='job-title'>Job title</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='frame-60'>
+        <div className='frame-61'>
+          <div className='frame-62'>
+            <div className='frame-63'>
+              <div className='frame-64'>
+                <div className='rectangle-65' />
+                <span className='consultation'>30 min Consultation</span>
+              </div>
+              <span className='business-needs'>
+                Dive into what your getting out, find the perfect time for us to
+                solve your business needs
+              </span>
+              <button className='button-66'>
+                <div className='state-layer-67'>
+                  <span className='book-now'>Book Now</span>
+                </div>
+              </button>
+            </div>
+            <div className='frame-68'>
+              <button className='rectangle-69' />
+              <span className='get-in-touch'>Get in touch </span>
+              <div className='frame-6a'>
+                <div className='frame-6b'>
+                  <div className='frame-6c'>
+                    <div className='input'>
+                      <span className='input-name-surname'>Name & Surname</span>
+                    </div>
+                    <div className='input-email'>
+                      <span className='input-company'>Email</span>
+                    </div>
+                  </div>
+                  <div className='input-message'>
+                    <span className='button-input'>Company</span>
+                  </div>
+                  <div className='button-group'>
+                    <span className='button-6d'>Massage</span>
+                  </div>
+                </div>
+              </div>
+              <button className='state-layer-6e'>
+                <div className='contact-us-6f'>
+                  <div className='footer-desktop-primary'>
+                    <div className='footer-content'>
+                      <span className='footer-logo'>Contact Us</span>
+                    </div>
+                  </div>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='logo-70'>
+        <div className='menu-71'>
+          <div className='menu-item'>
+            <span className='menu-item-72'>Logo</span>
+          </div>
+          <div className='wrapper-1f'>
+            <div className='section-17'>
+              <div className='box-13'>
+                <span className='item-73'>ITEM</span>
+              </div>
+              <div className='menu-item-74'>
+                <span className='item-1-75'>item-1</span>
+              </div>
+              <div className='menu-item-76'>
+                <span className='item-2-77'>item-2</span>
+              </div>
+              <div className='menu-item-78'>
+                <span className='item-3-79'>item-3</span>
+              </div>
+              <div className='menu-item-7a'>
+                <span className='item-4'>item-4</span>
+              </div>
+            </div>
+            <div className='menu-item-7b'>
+              <div className='menu-item-7c'>
+                <span className='item-7d'>ITEM</span>
+              </div>
+              <div className='menu-item-7e'>
+                <span className='item-1-7f'>item-1</span>
+              </div>
+              <div className='menu-item-80'>
+                <span className='item-2-81'>item-2</span>
+              </div>
+              <div className='menu-item-82'>
+                <span className='item-3-83'>item-3</span>
+              </div>
+              <div className='menu-item-84'>
+                <span className='item-4-85'>item-4</span>
+              </div>
+            </div>
+            <div className='menu-item-86'>
+              <div className='menu-item-87'>
+                <span className='item-88'>ITEM</span>
+              </div>
+              <div className='menu-item-89'>
+                <span className='item-1-8a'>item-1</span>
+              </div>
+              <div className='menu-item-8b'>
+                <span className='item-2-8c'>item-2</span>
+              </div>
+              <div className='menu-item-8d'>
+                <span className='item-3-8e'>item-3</span>
+              </div>
+              <div className='menu-item-8f'>
+                <span className='item-4-90'>item-4</span>
+              </div>
+            </div>
+          </div>
+          <div className='footer-right'>
+            <div className='text'>
+              <div className='lorem-ipsum-dolor'>
+                <span className='lorem-ipsum-dolor-91'>
+                  Lorem ipsum dolor sitamet, consectetur adipiscing elit.
+                  <br />
+                </span>
+                <span className='lorem-ipsum-dolor-92'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </span>
+              </div>
+            </div>
+            <div className='footer-icons'>
+              <div className='footer-icons-93'>
+                <div className='icons-instagram'>
+                  <div className='group-94'>
+                    <div className='vector-95' />
+                  </div>
+                </div>
+              </div>
+              <div className='footer-icons-96'>
+                <div className='icons-facebook'>
+                  <div className='vector-97' />
+                </div>
+              </div>
+              <div className='footer-icons-98'>
+                <div className='icons-twitter'>
+                  <div className='group-99'>
+                    <div className='vector-9a' />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='copyright'>
+          <span className='all-rights-reserved'>© All Rights Reserved</span>
+        </div>
+      </div>
     </div>
   );
 };
